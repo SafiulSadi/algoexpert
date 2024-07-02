694a69ca87e9950e295b2189d1e85e100058e5da
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2024-07-02 16:42:42 +0600

    initial commit 12

--- main.py
+++ main.py
@@ -1,17 +1,54 @@
-n = int(input())
-a = 0
-b = 0
-c = 0
-while n > 0:
-    arr = list(map(int, input().split()))
-    a += arr[0]
-    b += arr[1]
-    c += arr[2]
+# young physicist
+# n = int(input())
+# a = 0
+# b = 0
+# c = 0
+# while n > 0:
+#     arr = list(map(int, input().split()))
+#     a += arr[0]
+#     b += arr[1]
+#     c += arr[2]
+#
+#
+#     n -= 1
+#
+# if a == 0 and b == 0 and c == 0:
+#     print("YES")
+# else:
+#     print("NO")
+    #
+    # # beautiful matrix
+    # arr = []
+    # for i in range(5):
+    #     brr = list(map(int, input().split()))
+    #     arr.append(brr)
+    # #     print(brr)
+    # # print(arr)
+    # x = 0
+    # y = 0
+    # for i in range(5):
+    #     for j in range(5):
+    #         if arr[i][j] == 1:
+    #             x = i
+    #             y = j
+    # # print(f"x: {x} Y:{y}")
+    # print(f"{abs(2 - x) + abs(2 - y)}")
+
+abc = {1:5, 2:6}
+print(abc[1])
+
+arr = [1, 2, 3]
 
 
-    n -= 1
 
-if a == 0 and b == 0 and c == 0:
-    print("YES")
-else:
-    print("NO")
+
+
+
+
+
+
+
+
+
+
+
